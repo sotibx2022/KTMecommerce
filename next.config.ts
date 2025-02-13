f2d0b74beed9906fb9ed6e42835a856b1416be9a
Binaya <sbinayaraj@gmail.com>
2025-02-13 17:39:07 +0200

    .eslintignore file created

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,6 @@
 require('dotenv').config();
 module.exports = {
-  env: {
-    API_URL: process.env.API_URL,
-    CONNECTION_STRING:process.env.CONNECTION_STRING,
-  },
+
   eslint: {
     ignoreDuringBuilds: true,
   },
