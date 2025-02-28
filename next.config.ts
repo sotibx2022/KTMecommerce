41728c341b3cbe21a4b411c17ac147e418cb1401
Binaya <sbinayaraj@gmail.com>
2025-02-28 11:00:08 +0200

    target removed from next config

--- next.config.ts
+++ next.config.ts
@@ -2,5 +2,12 @@ module.exports = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  target:"serverless"
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*", // Matches any API request
+        destination: "https://ecommercektm.netlify.app/:path*", // Replace with your actual live API URL
+      },
+    ];
+  },
 };
