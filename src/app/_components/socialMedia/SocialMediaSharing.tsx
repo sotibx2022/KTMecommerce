ad7b318a5524452779539baa92aa209f87ea65ba
Binaya <sbinayaraj@gmail.com>
2025-04-21 16:54:22 +0300

    onClick event added to all of the social media icons

--- src/app/_components/socialMedia/SocialMediaSharing.tsx
+++ src/app/_components/socialMedia/SocialMediaSharing.tsx
@@ -12,18 +12,34 @@ interface IMediaSharing{
     productSlug:string
 }
 const SocialMediaSharing:React.FC<IMediaSharing> = ({productId,productSlug}) => {
-    const handleFacebookShare=()=>{
+    const handleShare=(mediaName:string)=>{
         const shareUrl = encodeURIComponent(`${config.websiteUrl}/singleProduct/id:${productId}&,slug:${productSlug}`);
-        const facebookShareLink = `https://www.facebook.com/sharer/sharer.php?u=${shareUrl}`;
-        window.open(facebookShareLink, "_blank", "noopener,noreferrer");
+        let shareLink=""
+        switch(mediaName){
+            case "facebook":
+                shareLink = `https://www.facebook.com/sharer/sharer.php?u=${shareUrl}`
+                break;
+                case "instagram":
+                    shareLink="";
+                    break;
+                case "whatsapp":
+                    shareLink=`https://wa.me/?text=${shareUrl}`;
+                    break;
+                case "twitter":
+                    shareLink= `https://twitter.com/intent/tweet?url=${shareUrl}&text=Check this out!`;
+                    break;
+                default:
+                    return
+        }
+        window.open(shareLink, "_blank", "noopener,noreferrer");
     }
     return (
         <div className='mt-2'>
             <div className="flex items-center justify-start space-x-6">
-                <FontAwesomeIcon icon={faFacebookSquare} size="2x" className="cursor-pointer text-blue-600 hover:text-blue-800 transition-colors duration-200" onClick={handleFacebookShare} />
-                <FontAwesomeIcon icon={faTwitterSquare} size="2x" className='cursor-pointer text-blue-400 hover:text-blue-600 transition-colors duration-200' />
-                <FontAwesomeIcon icon={faInstagram} size="2x" className='cursor-pointer text-pink-500 hover:text-pink-700 transition-colors duration-200' />
-                <FontAwesomeIcon icon={faWhatsappSquare} size="2x" className="cursor-pointer text-green-500 hover:text-green-700 transition-colors duration-200" />
+                <FontAwesomeIcon icon={faFacebookSquare} size="2x" className="cursor-pointer text-blue-600 hover:text-blue-800 transition-colors duration-200" onClick={()=>handleShare("facebook")} />
+                <FontAwesomeIcon icon={faTwitterSquare} size="2x" className='cursor-pointer text-blue-400 hover:text-blue-600 transition-colors duration-200' onClick={()=>handleShare("twitter")} />
+                <FontAwesomeIcon icon={faInstagram} size="2x" className='cursor-pointer text-pink-500 hover:text-pink-700 transition-colors duration-200' onClick={()=>handleShare("instagram")} />
+                <FontAwesomeIcon icon={faWhatsappSquare} size="2x" className="cursor-pointer text-green-500 hover:text-green-700 transition-colors duration-200" onClick={()=>handleShare("whatsapp")}/>
             </div>
         </div>
     );
