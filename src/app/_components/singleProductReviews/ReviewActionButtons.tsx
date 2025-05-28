a9ccf946b2a7b974697c4a07075783d3b0d7d5df
Binaya <sbinayaraj@gmail.com>
2025-05-28 10:48:09 +0300

    wishlist functionality completed

--- src/app/_components/singleProductReviews/ReviewActionButtons.tsx
+++ src/app/_components/singleProductReviews/ReviewActionButtons.tsx
@@ -11,6 +11,8 @@ import AddSingleProductReviews from './AddSingleProductReviews';
 import EditSingleProductReview from './EditSingleProductReview';
 import { IProductIdentifier } from '@/app/types/remarks';
 import useReviewDelete from '@/app/hooks/queryHooks/useMutation/useReviewDelete';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEdit, faTrashAlt } from '@fortawesome/free-solid-svg-icons';
 interface ReviewActionButtonsProps{
   productIdentifier:IProductIdentifier
 }
@@ -44,18 +46,14 @@ const mutation = useReviewDelete(userEmail,productId)
       className="p-2 text-blue-600 hover:text-blue-800 hover:bg-blue-50 rounded-full transition-colors"
       aria-label="Edit review"
     >
-      <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
-      </svg>
+       <FontAwesomeIcon icon={faEdit} className="w-5 h-5" />
     </button>
     <button
       onClick={deleteReview}
       className="p-2 text-red-600 hover:text-red-800 hover:bg-red-50 rounded-full transition-colors"
       aria-label="Delete review"
     >
-      <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
-      </svg>
+      <FontAwesomeIcon icon={faTrashAlt} className="w-5 h-5" />
     </button>
     {visibleComponent ==='dilaugeBox' && <DeleteConfirmation message='Are you Sure to Delete the Review' returnConfirmValue={getConfirmValue} loading={mutation.isPending}/>}
     {visibleComponent ==='editReview' && <EditSingleProductReview productIdentifier={productIdentifier}/>}
--- src/app/dashboard/wishlist/page.tsx
+++ src/app/dashboard/wishlist/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React from 'react';
+import React, { useContext } from 'react';
 import { useSelector } from 'react-redux';
 import { ReduxState } from '@/app/redux/store';
 import Link from 'next/link';
@@ -7,11 +7,48 @@ import PrimaryButton from '@/app/_components/primaryButton/PrimaryButton';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHeart, faTrashAlt } from '@fortawesome/free-solid-svg-icons';
 import { IWishListItem } from '@/app/types/wishlist';
+import SkeletonSlide from '@/app/_components/loadingComponent/SkeletonSlide';
+import LinkComponent from '@/app/_components/linkComponent/LinkComponent';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import axios from 'axios';
+import { UserDetailsContext } from '@/app/context/UserDetailsContextComponent';
+import { removeFromWishList } from '@/app/redux/wishListSlice';
 const wishListItemsPage = () => {
+  const context = useContext(UserDetailsContext);
+  if(!context){
+throw new Error ("User Details Context is not defined at this component")
+  }
+  const {userDetails} = context;
+  const userId = userDetails?._id;
+  const queryClient = useQueryClient()
+  const removeItemFromDataBase = async(productId:string) =>{
+const response = await axios.post('/api/wishList/removeFromWishList',productId);
+return response.data;
+  }
+  const deleteWishListMutation =useMutation({mutationFn:removeItemFromDataBase,
+    onSuccess:()=>{
+queryClient.invalidateQueries({ 
+      queryKey: ["wishListItems", userId],
+      refetchType: 'active',
+    });
+    }
+  })
+  const removeItemFromWishList =(productId:string) =>{
+    removeFromWishList(productId);
+deleteWishListMutation.mutate(productId)
+  }
   const { wishListItems,wishListLoading } = useSelector((state:ReduxState) => state.wishList);
+  if(wishListLoading){
+    return (<div className="grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+       <SkeletonSlide/>
+       <SkeletonSlide/>
+       <SkeletonSlide/>
+       <SkeletonSlide/>
+    </div>)
+  }
   return (
     <div className="min-h-screen py-8 px-4 sm:px-6 lg:px-8  w-full">
-  {wishListItems.length === 0 ? (
+  {!wishListLoading && wishListItems.length === 0 ? (
     <div className='w-full h-full text-red-500 flex flex-col items-center gap-4'>
       <p>There are no wish List Items Items Saved.</p>
       <Link href="/pages/isNewItems">
@@ -33,10 +70,7 @@ const wishListItemsPage = () => {
                 <div className="mt-4 flex justify-between">
                   <div>
                     <h3 className="text-sm text-primaryDark">
-                      <Link href={`/products/${item.productId}`}>
-                        <span aria-hidden="true" className="absolute inset-0" />
-                        {item.productName}
-                      </Link>
+                      <LinkComponent href={`/singleProduct/productIdentifier?id=${item.productId}&slug=${item.productName}`} text={item.productName}/>
                     </h3>
                     <p className="text-background bg-helper p-2 rounded-md text-center">{item.brand}</p>
                   </div>
@@ -49,6 +83,7 @@ const wishListItemsPage = () => {
                  <button
                        className="p-2 text-red-600 hover:text-red-800 hover:bg-red-50 rounded-full transition-colors"
                        aria-label="Delete review"
+                       onClick={()=>removeItemFromWishList(item.productId)}
                      >
                        <FontAwesomeIcon icon={faTrashAlt} className="w-5 h-5" />
                      </button>
