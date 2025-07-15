9c567e1f34775d05e9baf4d38d8fb28f4cb81303
Binaya <sbinayaraj@gmail.com>
2025-07-15 13:10:22 +0300

    delete notification logics added to the code

--- src/app/dashboard/notifications/page.tsx
+++ src/app/dashboard/notifications/page.tsx
@@ -13,11 +13,14 @@ import { formatDistanceToNow } from 'date-fns';
 import SkeletonNotifications from './SkeletonNotifications';
 import { motion } from 'framer-motion';
 import toast from 'react-hot-toast';
+import NoData from '@/app/_components/noData/NoData';
 const NotificationPage = () => {
   const [isNotificationUpdating, setIsNotificationUpdating] = useState(false);
-  const queryClient = useQueryClient()
   const [unread, setUnread] = useState(true);
   const [hoveredNotificationId, setHoveredNotificationId] = useState<number | null>(null);
+  const [isDeleteSettled, setIsDeleteSettled] = useState(true);
+  const [isUpdateSettled, setIsUpdateSettled] = useState(true);
+  const queryClient = useQueryClient();
   const getAllNotifications = async (): Promise<APIResponseSuccess<INotificationDisplay[]> | APIResponseError> => {
     try {
       const response = await axios.get('/api/notifications');
@@ -26,15 +29,6 @@ const NotificationPage = () => {
       return { message: "Error To get notifications", status: 400, success: false };
     }
   };
-  const { data, isPending } = useQuery({
-    queryKey: ['notifications'],
-    queryFn: getAllNotifications
-  });
-  const calculateDuration = (date: Date) => formatDistanceToNow(date, { addSuffix: true });
-  const notifications = data?.success ? data.data : [];
-  const filteredNotifications = unread
-    ? notifications?.filter(notification => !notification.read)
-    : notifications?.filter(notification => notification.read);
   const updateNotification = async ({ notificationId, readValue }: NotificationDatatoUpdate): Promise<APIResponseSuccess | APIResponseError> => {
     try {
       setIsNotificationUpdating(true)
@@ -62,69 +56,98 @@ const NotificationPage = () => {
     AxiosError,
     NotificationDatatoUpdate
   >({
-    mutationFn: updateNotification,
-    onSuccess: (data) => {
+    mutationFn: async (params) => {
+      setIsUpdateSettled(false);
+      return await updateNotification(params);
+    },
+    onSuccess: async (data) => {
       if (data.success) {
-        queryClient.invalidateQueries({
+        await queryClient.refetchQueries({
           queryKey: ['notifications'],
-          refetchType: 'active'
+          exact: true,
         });
-        setIsNotificationUpdating(false);
       } else {
-        setIsNotificationUpdating(false);
         console.error(data.message);
       }
     },
     onError: (error) => {
       console.error("Mutation error:", error.message);
-      // Optionally show a toast notification
+    },
+    onSettled: () => {
+      setIsUpdateSettled(true);
     }
   });
   const deleteNotification = useMutation({
     mutationFn: async (notificationId: string) => {
+      setIsDeleteSettled(false);
       const response = await axios.delete(`/api/notifications/${notificationId}`);
-      return response.data
+      return response.data;
     },
-    onSuccess: () => {
-      toast.success("Notification Deleted Successfully")
-      queryClient.invalidateQueries({
+    onSuccess: async () => {
+      toast.success("Notification Deleted Successfully");
+      await queryClient.refetchQueries({
         queryKey: ['notifications'],
-        refetchType: 'active'
+        exact: true,
       });
     },
     onError: () => {
-      toast.error("Notification Can't be Deleted.")
+      toast.error("Notification Can't be Deleted.");
+    },
+    onSettled: () => {
+      setIsDeleteSettled(true);
     }
-  })
-  if (isPending || isNotificationUpdating || deleteNotification.isPending) {
+  });
+  const { data, isPending } = useQuery({
+    queryKey: ['notifications'],
+    queryFn: getAllNotifications
+  });
+  const notifications = data?.success ? data.data : [];
+  const filteredNotifications = unread
+    ? notifications?.filter(notification => !notification.read)
+    : notifications?.filter(notification => notification.read);
+  const calculateDuration = (date: string) => {
+    return formatDistanceToNow(new Date(date), { addSuffix: true });
+  };
+const unreadNotifications = notifications?.filter(n => !n.read) ?? [];
+const readNotifications = notifications?.filter(n => n.read) ?? [];
+  if (isPending || !isUpdateSettled || !isDeleteSettled) {
     return <SkeletonNotifications />;
   }
   return (
     <div className="max-w-2xl mx-auto p-4 w-[400px]">
-      <div className="notificationTitle flex items-center justify-between mb-6">
-        <div className="flex items-center">
-          <Bell className="h-6 w-6 text-helper mr-2" />
-          <h1 className="text-2xl font-bold text-primaryDark">Notifications</h1>
-        </div>
-        <div className="notificationToggleArea flex items-center gap-6">
-          <div
-            className={`relative flex items-center gap-1 border border-helper p-2 rounded-lg text-primaryDark cursor-pointer hover:bg-helper hover:text-background duration-300 ${!unread ? 'bg-helper' : ''}`}
-            onClick={() => setUnread(unread)}
-          >
-            <MailOpen className="h-5 w-5" />
-            <span className='primaryParagraph'>{notifications && notifications?.length - (filteredNotifications ? filteredNotifications?.length : 0)}</span>
-            <h3 className="text-sm font-medium">Read</h3>
+      <div className="notificationTitle flex flex-col mb-6">
+        <div className="flex items-center justify-between w-full">
+          <div className="flex items-center">
+            <Bell className="h-6 w-6 text-helper mr-2" />
+            <h1 className="text-2xl font-bold text-primaryDark">Notifications</h1>
           </div>
-          <div
-            className={`flex items-center gap-2 border border-helper p-2 rounded-lg text-primaryDark cursor-pointer hover:bg-helper hover:text-background duration-300 ${unread ? 'bg-helper' : ''}`}
-            onClick={() => setUnread(!unread)}
-          >
-            <Mail className="h-5 w-5" />
-            <h3 className="text-sm font-medium">Unread</h3>
-            <span className='primaryParagraph absolute top-1/2 left-1/2 -translate-1/2'>{filteredNotifications ? filteredNotifications?.length : 0}</span>
+          <div className="notificationToggleArea flex gap-1">
+            <div
+              className={` flex items-center gap-1  text-primaryDark cursor-pointer hover:text-helper duration-300 ${unread ? 'text-helper' : 'text-primaryLight border-2 border-transparent border-b-primaryLight'
+                }`}
+              onClick={() => setUnread(false)}
+            >
+              <MailOpen className="h-5 w-5" />
+              <h3 className="text-sm font-medium">Read</h3>
+              <span>
+                {readNotifications.length}
+              </span>
+            </div>
+            <div
+              className={` flex items-center gap-1  text-primaryDark cursor-pointer hover:text-helper duration-300 ${!unread ? 'text-helper' : 'text-primaryLight border-2 border-transparent border-b-primaryLight'
+                }`}
+              onClick={() => setUnread(true)}
+            >
+              <Mail className="h-5 w-5" />
+              <h3 className="text-sm font-medium">Unread</h3>
+              <span className=' '>
+                {unreadNotifications.length}
+              </span>
+            </div>
           </div>
         </div>
       </div>
+      {/* Notification List */}
       <div className="space-y-4">
         {filteredNotifications && filteredNotifications.length > 0 ? (
           filteredNotifications.map((notification, index) => (
@@ -144,22 +167,25 @@ const NotificationPage = () => {
                   </p>
                   <div className="mt-2 flex items-center justify-between">
                     <span className="text-xs text-primaryLight">
-                      {calculateDuration(notification.createdAt)}
+                      {calculateDuration(notification.createdAt.toString())}
                     </span>
-                    {hoveredNotificationId === index &&
+                    {hoveredNotificationId === index && (
                       <motion.div
                         initial={{ top: "100%" }}
                         animate={{ top: 0 }}
                         exit={{ top: "100%" }}
                         transition={{ duration: 0.3 }}
-                        className="absolute w-full h-[20px] bg-helper overflow-hidden left-0 flex justify-center items-center">
+                        className="absolute w-full h-[20px] bg-helper overflow-hidden left-0 flex justify-center items-center"
+                      >
                         <div className="buttonCollections flex items-center gap-4">
-                          <button className='text-background'
+                          <button
+                            className='text-background'
                             onClick={() => changeNotificationReadMutation.mutate({
                               notificationId: notification._id.toString(),
                               readValue: notification.read
                             })}
-                          > {!notification.read ? "Mark Read" : "Mark UnRead"}
+                          >
+                            {!notification.read ? "Mark Read" : "Mark UnRead"}
                           </button>
                           <button
                             className="text-red-500 hover:text-red-700"
@@ -169,16 +195,19 @@ const NotificationPage = () => {
                           </button>
                         </div>
                       </motion.div>
-                    }
+                    )}
                   </div>
                 </div>
               </div>
             </motion.div>
           ))
         ) : (
-          <div className="text-center py-8 text-primaryLight">
-            <h1>There are no notifications</h1>
-          </div>
+          <NoData
+            icon={<Bell className="w-12 h-12 text-red-500" strokeWidth={1.5} />}
+            notFoundMessage="No notifications found"
+            buttonText="Browse"
+            buttonLink="/catalog/advanceSearch?highlighted=none"
+          />
         )}
       </div>
     </div>
