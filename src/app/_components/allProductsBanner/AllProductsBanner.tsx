e0dfe0b535f83bc162a014791ed9c3d2c58592a0
Binaya <sbinayaraj@gmail.com>
2025-07-20 10:49:56 +0300

    text 2 from banner header removed

--- src/app/_components/allProductsBanner/AllProductsBanner.tsx
+++ src/app/_components/allProductsBanner/AllProductsBanner.tsx
@@ -33,7 +33,6 @@ const AllProductsBanner = () => {
       />
       <BannerHeading 
         text1="Explore Our Full Range" 
-        text2="Discover All Innovations"
       />
       <Link href='/catalog/advanceSearch?category=mobile'>
       <PrimaryButton searchText='Browse'/></Link>
--- src/app/_components/allProductsBanner/BannerHeading.tsx
+++ src/app/_components/allProductsBanner/BannerHeading.tsx
@@ -2,16 +2,12 @@
 import React from 'react'
 interface IBannerHeading{
   text1:string;
-  text2:string;
 }
-const BannerHeading:React.FC<IBannerHeading> = ({ text1, text2 }) => {
+const BannerHeading:React.FC<IBannerHeading> = ({ text1}) => {
   return (
     <div className='flex-center flex flex-col justify-center items-center  mb-1 font-bold'>
       <h1 className='inline-block bg-gradient-to-r text-xl sm:text-4xl from-primaryDark to-primaryLight text-transparent bg-clip-text'>
         {text1}
-      </h1>
-      <h1 className='inline-block bg-gradient-to-r text-lg sm:text-xl from-helper to-[#f38d07] text-transparent bg-clip-text uppercase tracking-wider'>
-        {text2}
       </h1>
     </div>
   )
