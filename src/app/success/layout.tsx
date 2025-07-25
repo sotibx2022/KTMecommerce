9b89412934254ebb14c2d43dc21083cdd965fe8c
Binaya <sbinayaraj@gmail.com>
2025-07-25 18:25:53 +0300

    pagesLayout added to the success page

---
+++ src/app/success/layout.tsx
@@ -0,0 +1,17 @@
+import type { Metadata } from "next";
+import "./../globals.css";
+import PagesLayout from "../pages/PagesLayout";
+export const metadata: Metadata = {
+  title:"Payment Confirmation Status - KTMEcommerce"
+};
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+       <PagesLayout>
+        {children}
+       </PagesLayout>
+  );
+}
