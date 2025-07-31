4501cf2124de9512acdefa58239aee561adcfad9
Binaya <sbinayaraj@gmail.com>
2025-08-01 02:37:55 +0300

    router.refresh cause redirection to the home page

--- src/app/_components/authComponent/AdminLogin.tsx
+++ src/app/_components/authComponent/AdminLogin.tsx
@@ -40,7 +40,7 @@ const AdminLogin = () => {
     onSuccess: (response) => {
       if (response.success) {
         toast.success(response.message);
-        router.refresh()
+        router.push('/admin')
       } else {
         toast.error(response.message)
       }
