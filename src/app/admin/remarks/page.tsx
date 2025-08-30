aad1bf5b309180bf2b54052cacc355cc3313e36b
Binaya <sbinayaraj@gmail.com>
2025-08-30 10:16:46 +0300

    remarks page is client page at admin

--- src/app/admin/remarks/page.tsx
+++ src/app/admin/remarks/page.tsx
@@ -1,3 +1,4 @@
+"use client"
 import React from 'react'
 import TotalReviews from './reviewsComponent/TotalReviews'
 import UnderDevelopment from '@/app/_components/UnderDevelopment/UnderDevelopment'
