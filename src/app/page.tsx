66bca9883b3ae339d4619923ffe7a14760d6622f
Binaya <sbinayaraj@gmail.com>
2025-08-31 23:39:49 +0300

    home page data fetching completed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,37 +8,40 @@ import axios from 'axios';
 import { SpecificProducts } from './services/apiFunctions/productsQuery';
 const page = async () => {
   const queryClient = getQueryClient();
-  await queryClient.prefetchQuery({
+await Promise.all([
+  // Prefetch categories
+  queryClient.prefetchQuery({
     queryKey: ["initialCategories"],
     queryFn: fetchInitialCategories,
-  })
-  await queryClient.prefetchQuery({
+  }),
+  // Prefetch user details
+  queryClient.prefetchQuery({
     queryKey: ['user'],
     queryFn: getUserDetails,
-  })
-  await queryClient.prefetchQuery({
+  }),
+  // Prefetch brands
+  queryClient.prefetchQuery({
+    queryKey: ['brands'],
     queryFn: async () => {
       const response = await axios.get('/api/categories/brand');
-      return response.data.brandsArray
+      return response.data.brandsArray;
     },
-    queryKey: ['brands'],
+  }),
+  ...["isTrendingNow","isNewArrival","isTopSell","isOfferItem"].map((categoryItem:string)=>{
+queryClient.prefetchQuery({
+    queryKey: ['categoryType', categoryItem],
+    queryFn: () => SpecificProducts(categoryItem, "1", "8")
   })
-  await queryClient.prefetchQuery({
-    queryKey: ['categoryType', "isTrendingNow"],
-    queryFn: () => SpecificProducts("isTrendingNow", "1", "8")
+  }),
+  // Prefetch sliders data
+  queryClient.prefetchQuery({
+    queryKey: ['slidersData'],
+    queryFn: async () => {
+      const response = await axios.get('/api/sliders');
+      return response.data.data;
+    }
   })
-  await queryClient.prefetchQuery({
-    queryKey: ['categoryType', "isNewArrival"],
-    queryFn: () => SpecificProducts("isNewArrival", "1", "8")
-  })
-  await queryClient.prefetchQuery({
-    queryKey: ['categoryType', "isTopSell"],
-    queryFn: () => SpecificProducts("isTopSell", "1", "8")
-  })
-  await queryClient.prefetchQuery({
-    queryKey: ['categoryType', "isOfferItem"],
-    queryFn: () => SpecificProducts("isOfferItem", "1", "8")
-  })
+]);
   const dehydratedState = dehydrate(queryClient)
   return (
     <HydrationBoundary state={dehydratedState}>
