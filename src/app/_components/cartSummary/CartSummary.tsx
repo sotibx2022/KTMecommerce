5900acf52afc286a299915d3f1b0c3fb2ddda06b
Binaya <sbinayaraj@gmail.com>
2025-09-03 12:08:21 +0300

    logic for wishlist and cartlist completed

--- src/app/_components/cartSummary/CartSummary.tsx
+++ src/app/_components/cartSummary/CartSummary.tsx
@@ -1,6 +1,6 @@
-import React from 'react'
-import PrimaryButton from '../primaryButton/PrimaryButton'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import React from 'react';
+import PrimaryButton from '../primaryButton/PrimaryButton';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSquareCheck } from '@fortawesome/free-solid-svg-icons';
 import { useSelector } from 'react-redux';
 import { CartState } from '@/app/redux/cartSlice';
@@ -9,90 +9,101 @@ import { calculateTotals } from '@/app/services/helperFunctions/cartFunctions';
 import { IOrderItem } from '@/app/types/orders';
 import { Rupee } from '../productCard/Rupee';
 interface ICartSummary {
-    order?: boolean
-    items?: IOrderItem[]
+  order?: boolean;
+  items?: IOrderItem[];
 }
 const CartSummary: React.FC<ICartSummary> = ({ order, items }) => {
-    const { cartItems, isInitialized } = useSelector((state: { cart: CartState }) => state.cart);
-    const datatoRender = items || cartItems;
-    const router = useRouter();
-    const handleOrderProducts = () => {
-        router.push(`/dashboard/cartProcess`)
-    }
-    // Calculate totals or return loading state
-    const { totalItems, totalCost, discount, shippingPrice, grossTotal } = !isInitialized && !items
-        ? {
-            totalItems: 0,
-            totalCost: 0,
-            discount: 0,
-            shippingPrice: 0,
-            grossTotal: 0
+  const { cartItems, isInitialized } = useSelector(
+    (state: { cart: CartState }) => state.cart
+  );
+  const datatoRender = items || cartItems;
+  const router = useRouter();
+  const handleOrderProducts = () => {
+    router.push(`/dashboard/cartProcess`);
+  };
+  // Calculate totals or fallback if not initialized
+  const { totalItems, totalCost, discount, shippingPrice, grossTotal } =
+    !isInitialized && !items
+      ? {
+          totalItems: 0,
+          totalCost: 0,
+          discount: 0,
+          shippingPrice: 0,
+          grossTotal: 0,
         }
-        : calculateTotals(datatoRender);
-    // Helper function to display values with loading state
-    const displayValue = (value: number, isCurrency = false) => {
-        if (!items) return "Loading...";
-        return isCurrency ? (
-            <span>
-                <Rupee price={value} />
+      : calculateTotals(datatoRender);
+  // Helper function to display values with loading state
+  const displayValue = (value: number, isCurrency = false) => {
+    if (!items && !isInitialized) return "Loading...";
+    return isCurrency ? <Rupee price={value} /> : value.toString();
+  };
+  return (
+    <div className="CartSummary flex flex-col gap-2 my-5 max-w-[500px]">
+      {datatoRender === cartItems && isInitialized && (
+        <h2 className="subHeading">Cart Summary</h2>
+      )}
+      <div className="summaryLine flex justify-between items-center border-b-2 border-dotted border-helper">
+        <span className="cartSummaryTitle secondaryHeading">
+          <FontAwesomeIcon icon={faSquareCheck} className="mr-2" />
+          Total Items
+        </span>
+        <span className="cartSummaryData secondaryHeading">
+          {displayValue(totalItems)}
+        </span>
+      </div>
+      <div className="summaryLine flex justify-between items-center border-b-2 border-dotted border-helper">
+        <span className="cartSummaryTitle secondaryHeading">
+          <FontAwesomeIcon icon={faSquareCheck} className="mr-2" />
+          Total Cost
+        </span>
+        <span className="cartSummaryData secondaryHeading">
+          {displayValue(totalCost, true)}
+        </span>
+      </div>
+      <div className="summaryLine flex justify-between items-center border-b-2 border-dotted border-helper">
+        <div className="cartSummaryTitle">
+          <p className="secondaryHeading">
+            <FontAwesomeIcon icon={faSquareCheck} className="mr-2" />
+            Discount
+          </p>
+          {datatoRender === cartItems && isInitialized && (
+            <span className="primaryParagraph italic text-green-500">
+              10% off above $200
             </span>
-        ) : (
-            value.toString()
-        );
-    };
-    return (
-        <div className="CartSummary flex flex-col gap-2 my-5 max-w-[500px]">
-            {datatoRender === cartItems && isInitialized && <h2 className="subHeading">Cart Summary</h2>}
-            <div className="summaryLine flex justify-between items-center border-b-2 border-dotted border-helper">
-                <span className="cartSummaryTitle secondaryHeading">
-                    <FontAwesomeIcon icon={faSquareCheck} className="mr-2" />Total Items
-                </span>
-                <span className="cartSummaryData secondaryHeading">
-                    {displayValue(totalItems)}
-                </span>
-            </div>
-            <div className="summaryLine flex justify-between items-center border-b-2 border-dotted border-helper">
-                <span className="cartSummaryTitle secondaryHeading">
-                    <FontAwesomeIcon icon={faSquareCheck} className="mr-2" />Total Cost
-                </span>
-                <span className="cartSummaryData secondaryHeading">
-                    {displayValue(totalCost, true)}
-                </span>
-            </div>
-            <div className="summaryLine flex justify-between items-center border-b-2 border-dotted border-helper">
-                <div className="cartSummaryTitle">
-                    <p className="secondaryHeading">
-                        <FontAwesomeIcon icon={faSquareCheck} className="mr-2" />Discount
-                    </p>
-                    {datatoRender === cartItems && isInitialized && (
-                        <span className="primaryParagraph italic text-green-500">10% off above $200</span>
-                    )}
-                </div>
-                <span className="cartSummaryData secondaryHeading">
-                    {displayValue(discount, true)}
-                </span>
-            </div>
-            <div className="summaryLine flex justify-between items-center border-b-2 border-dotted border-helper">
-                <div className="cartSummaryTitle">
-                    <p className="secondaryHeading">
-                        <FontAwesomeIcon icon={faSquareCheck} className="mr-2" />Shipping Price
-                    </p>
-                    {datatoRender === cartItems && isInitialized && (
-                        <span className="primaryParagraph italic text-green-500">Free shipping over $1000</span>
-                    )}
-                </div>
-                <span className="cartSummaryData secondaryHeading">
-                    {displayValue(shippingPrice, true)}
-                </span>
-            </div>
-            <div className="summaryLine flex justify-between items-center">
-                <span className="cartSummaryTitle secondaryHeading uppercase">Gross Total</span>
-                <span className="cartSummaryData secondaryHeading price-highlight">
-                    {displayValue(grossTotal, true)}
-                </span>
-            </div>
-            {order && isInitialized && <PrimaryButton searchText="Order" onClick={handleOrderProducts} />}
+          )}
         </div>
-    )
-}
-export default CartSummary
+        <span className="cartSummaryData secondaryHeading">
+          {displayValue(discount, true)}
+        </span>
+      </div>
+      <div className="summaryLine flex justify-between items-center border-b-2 border-dotted border-helper">
+        <div className="cartSummaryTitle">
+          <p className="secondaryHeading">
+            <FontAwesomeIcon icon={faSquareCheck} className="mr-2" />
+            Shipping Price
+          </p>
+          {datatoRender === cartItems && isInitialized && (
+            <span className="primaryParagraph italic text-green-500">
+              Free shipping over $1000
+            </span>
+          )}
+        </div>
+        <span className="cartSummaryData secondaryHeading">
+          {displayValue(shippingPrice, true)}
+        </span>
+      </div>
+      <div className="summaryLine flex justify-between items-center">
+        <span className="cartSummaryTitle secondaryHeading uppercase">
+          Gross Total
+        </span>
+        <span className="cartSummaryData secondaryHeading price-highlight">
+          {displayValue(grossTotal, true)}
+        </span>
+      </div>
+      {order && isInitialized && (
+        <PrimaryButton searchText="Order" onClick={handleOrderProducts} />
+      )}
+    </div>
+  );
+};
+export default CartSummary;
--- src/app/_components/navbar/RegisteredUsersOption.tsx
+++ src/app/_components/navbar/RegisteredUsersOption.tsx
@@ -1,37 +1,91 @@
-"use client"
-import React, { useState, useEffect } from 'react';
-import SecondaryButton from '../secondaryButton/SecondaryButton';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCaretDown, faCaretUp, faSignOutAlt } from '@fortawesome/free-solid-svg-icons';
-import UserOptions from './UserOptions';
-import { useDispatch, useSelector } from 'react-redux';
-import { CartState, clearCartItems, setCart, setIsInitialized } from '@/app/redux/cartSlice';
-import { clearWishListItems } from '@/app/redux/wishListSlice';
-import { useUserDetails } from '@/app/context/UserDetailsContextComponent';
-import UserProfileImage from './UserProfileImage';
-import { useLogout } from '@/app/hooks/queryHooks/useLogout';
-import { useCartItems } from '@/app/hooks/queryHooks/useCartItems';
+"use client";
+import React, { useState, useEffect } from "react";
+import SecondaryButton from "../secondaryButton/SecondaryButton";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCaretDown, faCaretUp, faSignOutAlt } from "@fortawesome/free-solid-svg-icons";
+import UserOptions from "./UserOptions";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  CartState,
+  clearCartItems,
+  setCart,
+  setIsInitialized as setCartInitialized,
+} from "@/app/redux/cartSlice";
+import {
+  clearWishListItems,
+  IWishListState,
+  setWishList,
+} from "@/app/redux/wishListSlice";
+import { useUserDetails } from "@/app/context/UserDetailsContextComponent";
+import UserProfileImage from "./UserProfileImage";
+import { useLogout } from "@/app/hooks/queryHooks/useLogout";
+import { useCartItems } from "@/app/hooks/queryHooks/useCartItems";
+import { useWishListItems } from "@/app/hooks/queryHooks/useWishListItems";
 const RegisteredUsersOption = () => {
   const [showUserOptions, setShowUserOptions] = useState(false);
   const { userDetails } = useUserDetails();
   const logout = useLogout();
   const dispatch = useDispatch();
-  const { cartItems, isInitialized } = useSelector(
-    (state: { cart: CartState }) => state.cart
-  );
-  const { data: cartDetails, isPending, error } = useCartItems();
+  const { cartItems } = useSelector((state: { cart: CartState }) => state.cart);
+  const { wishListItems } = useSelector((state: { wishList: IWishListState }) => state.wishList);
+  const {
+    data: cartDetails,
+    isPending: isCartPending,
+  } = useCartItems();
+  const {
+    data: wishlistDetails,
+    isPending: isWishListPending,
+  } = useWishListItems();
   // Initialize cart or mark initialized
   useEffect(() => {
-    if(isPending){
-       dispatch(setIsInitialized(false));
+    if (!userDetails) {
+      dispatch(setCartInitialized(true));
+      return;
     }
-    else{
+    if (isCartPending) {
+      dispatch(setCartInitialized(false));
+    } else {
       if (cartDetails?.success && cartDetails.data && cartItems.length === 0) {
         dispatch(setCart(cartDetails.data));
-        dispatch(setIsInitialized(true));
       }
+      dispatch(setCartInitialized(true));
     }
-  }, [cartDetails, dispatch, isPending, userDetails, cartItems]);
+  }, [cartDetails, dispatch, isCartPending, userDetails, cartItems]);
+  // Initialize wishlist or mark initialized
+  useEffect(() => {
+    if (!userDetails) {
+      dispatch(setWishList({
+            wishListItems: [],
+            wishListLoading: false,
+          }));
+      return;
+    }
+    if (isWishListPending) {
+      dispatch(
+          setWishList({
+            wishListItems: [],
+            wishListLoading: true,
+          })
+        );
+    } else {
+      if (
+        wishlistDetails?.success &&
+        wishlistDetails.data &&
+        wishListItems.length === 0
+      ) {
+        dispatch(
+          setWishList({
+            wishListItems: wishlistDetails.data,
+            wishListLoading: false,
+          })
+        );
+      }
+     dispatch(setWishList({
+            wishListItems: [],
+            wishListLoading: false,
+          }));
+    }
+  }, [wishlistDetails, dispatch, isWishListPending, userDetails, wishListItems]);
   const handleLogout = () => {
     dispatch(clearCartItems());
     dispatch(clearWishListItems());
@@ -46,9 +100,10 @@ const RegisteredUsersOption = () => {
       >
         <UserProfileImage />
         <p className="text-white capitalize">
-          <span className='text-helper'>Welcome</span> {
-            userDetails && (userDetails.fullName.split(" ")[0] || userDetails.email.split("@")[0])
-          }
+          <span className="text-helper">Welcome</span>{" "}
+          {userDetails &&
+            (userDetails.fullName.split(" ")[0] ||
+              userDetails.email.split("@")[0])}
         </p>
         <FontAwesomeIcon
           icon={showUserOptions ? faCaretUp : faCaretDown}
@@ -62,6 +117,6 @@ const RegisteredUsersOption = () => {
         onClick={handleLogout}
       />
     </div>
-  )
-}
+  );
+};
 export default RegisteredUsersOption;
