7498474a2f7cf4f1f522601e3ec160b4afdc5e8a
Binaya <sbinayaraj@gmail.com>
2025-09-16 12:27:38 +0300

    logo component updated with tailwind css for conditional rendering

--- src/app/_components/logoComponent/LogoComponent.tsx
+++ src/app/_components/logoComponent/LogoComponent.tsx
@@ -1,38 +1,33 @@
-"use client"
-import { useScreenWidth } from '@/app/services/helperFunctions/findScreenWidth'
-import Link from 'next/link'
-import React from 'react'
+"use client";
+import Link from "next/link";
+import React from "react";
 interface LogoComponentProps {
-    theme?: "light" | "dark"
+  theme?: "light" | "dark";
 }
 const LogoComponent: React.FC<LogoComponentProps> = ({ theme }) => {
-    const screenWidth = useScreenWidth()
-    return (
-        <Link href="/">
-            {screenWidth > 1000 ? (
-                <div className='flex align-items-center gap-2'>
-                    <img
-                        src='/assets/brand/mobilelogo.png'
-                        alt='mobile logo'
-                        style={theme === "dark" ? { filter: 'invert(1) brightness(2)' } : undefined}
-                        className='w-auto h-[50px]'
-                    />
-                    <img
-                        src='/assets/brand/textlogo.png'
-                        alt='text logo'
-                        style={theme === "dark" ? { filter: 'invert(1) brightness(2)' } : undefined}
-                        className='w-auto h-[30px]'
-                    />
-                </div>
-            ) : (
-                <img
-                    src='/assets/brand/mobilelogo.png'
-                    alt='mobile logo'
-                    style={theme === "dark" ? { filter: 'invert(1) brightness(2)' } : undefined}
-                    className='w-auto h-[50px]'
-                />
-            )}
-        </Link>
-    )
-}
-export default LogoComponent
+  return (
+    <Link href="/">
+      <div className="hidden md:flex md:items-center md:gap-2">
+        <img
+          src="/assets/brand/mobilelogo.png"
+          alt="mobile logo"
+          style={theme === "dark" ? { filter: "invert(1) brightness(2)" } : undefined}
+          className="w-auto h-[50px]"
+        />
+        <img
+          src="/assets/brand/textlogo.png"
+          alt="text logo"
+          style={theme === "dark" ? { filter: "invert(1) brightness(2)" } : undefined}
+          className="w-auto h-[30px]"
+        />
+      </div>
+      <img
+        src="/assets/brand/mobilelogo.png"
+        alt="mobile logo"
+        style={theme === "dark" ? { filter: "invert(1) brightness(2)" } : undefined}
+        className="w-auto h-[50px] md:hidden"
+      />
+    </Link>
+  );
+};
+export default LogoComponent;
