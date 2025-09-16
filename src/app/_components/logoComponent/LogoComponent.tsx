ea905d5ef96837b2d527867ddc365b9575419c3e
Binaya <sbinayaraj@gmail.com>
2025-09-16 12:01:37 +0300

    project logo updated

--- src/app/_components/logoComponent/LogoComponent.tsx
+++ src/app/_components/logoComponent/LogoComponent.tsx
@@ -9,12 +9,29 @@ const LogoComponent: React.FC<LogoComponentProps> = ({ theme }) => {
     const screenWidth = useScreenWidth()
     return (
         <Link href="/">
-            <img
-                src={screenWidth > 1000 ? '/assets/brand/logo.png' : '/assets/brand/mobilelogo.png'}
-                alt="Brand Logo"
-                className="w-auto h-[50px]"
-                style={theme === "dark" ? { filter: 'invert(1) brightness(2)' } : undefined}
-            />
+            {screenWidth > 1000 ? (
+                <div className='flex align-items-center gap-2'>
+                    <img
+                        src='/assets/brand/mobilelogo.png'
+                        alt='mobile logo'
+                        style={theme === "dark" ? { filter: 'invert(1) brightness(2)' } : undefined}
+                        className='w-auto h-[50px]'
+                    />
+                    <img
+                        src='assets/brand/textlogo.png'
+                        alt='text logo'
+                        style={theme === "dark" ? { filter: 'invert(1) brightness(2)' } : undefined}
+                        className='w-auto h-[50px]'
+                    />
+                </div>
+            ) : (
+                <img
+                    src='/assets/brand/mobilelogo.png'
+                    alt='mobile logo'
+                    style={theme === "dark" ? { filter: 'invert(1) brightness(2)' } : undefined}
+                    className='w-auto h-[50px]'
+                />
+            )}
         </Link>
     )
 }
